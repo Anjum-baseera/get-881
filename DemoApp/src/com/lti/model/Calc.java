5bbfee09eb6f72af3202324f88cfe93e71803f4d
Farha <anjumsyed59@gmail.com>
2020-12-18 13:43:44 +0530

    Added div method in Calc class

--- DemoApp/src/com/lti/model/Calc.java
+++ DemoApp/src/com/lti/model/Calc.java
@@ -10,5 +10,8 @@ public class Calc {
 	public void mul(int num1,int num2) {
 		System.out.println("mul= "+(num1*num2));
 	}
+	public void div(int num1,int num2) {
+		System.out.println("div= "+(num1/num2));
+	}
 
 }
