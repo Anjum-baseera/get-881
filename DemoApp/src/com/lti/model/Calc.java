5c5dbb7c81eccc9e0a736a73820a8e05abee5d7f
Farha <anjumsyed59@gmail.com>
2020-12-18 15:53:21 +0530

    latest commit

--- DemoApp/src/com/lti/model/Calc.java
+++ DemoApp/src/com/lti/model/Calc.java
@@ -4,9 +4,7 @@ public class Calc {
 	public void add(int num1,int num2) {
 		System.out.println("Sum= "+(num1+num2));
 	}
-	public void subs(int num1,int num2) {
-		System.out.println("sub= "+(num1-num2));
-	}
+	
 
 
 }
